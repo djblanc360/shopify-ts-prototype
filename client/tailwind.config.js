1beb8c560617e11e0547dca124f0020493574f50
djblanc360 <djblanc360@gmail.com>
2024-09-17 16:03:13 -0700

    added aliased imports using tsconfig.json and vite.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,69 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.liquid',           // Watch for Tailwind classes in all .liquid files
+    './src/**/*.{js,ts,jsx,tsx}',  // Include source files for Tailwind classes
+    '!./node_modules/**/*'      // Exclude node_modules
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#25293b',
+        'secondary': '#989898',
+        'black': '#000000',
+        'grey': '#808080',
+        'ivory': '#FFFFF0',
+        'vintage-navy': '#001F3F',
+        'white': '#FFFFFF',
+        'heather-grey': '#B0B0B0',
+        'dry-sage': '#B2AC88',
+        'dark-taupe': '#483C32',
+        'shell': '#F3E5AB',
+        'navy': '#000080',
+        'driftwood': '#9E7E5C',
+        'ash': '#B2BEB5',
+        'willow': '#8A9A5B',
+        'sea': '#2E8B57',
+        'dune': '#D2B48C',
+        'cadet': '#5F9EA0',
+        'storm': '#708090',
+        'palm': '#0C5C3E',
+        'red-clay': '#9E453C',
+        'dark-charcoal': '#333333',
+        'coffee': '#6F4E37',
+        'latte': '#D3AE70',
+        'vintage-clay': '#B66A50'
+      },
+      fontFamily: {
+        karla: ['Karla', 'sans-serif'],
+      },
+    }
+  },
+  safelist: [
+    'bg-black',
+    'bg-grey',
+    'bg-ivory',
+    'bg-vintage-navy',
+    'bg-white',
+    'bg-heather-grey',
+    'bg-dry-sage',
+    'bg-dark-taupe',
+    'bg-shell',
+    'bg-navy',
+    'bg-driftwood',
+    'bg-ash',
+    'bg-willow',
+    'bg-sea',
+    'bg-dune',
+    'bg-cadet',
+    'bg-storm',
+    'bg-palm',
+    'bg-red-clay',
+    'bg-dark-charcoal',
+    'bg-coffee',
+    'bg-latte',
+    'bg-vintage-clay',
+    { pattern: /grid-cols-(\d+)/, variants: ['md', 'lg', 'xl'] }
+  ],
+  plugins: [],                  // Add Tailwind plugins here if required
+};
